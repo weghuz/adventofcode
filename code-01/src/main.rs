30f54468253a7932edeb9fed65fee1ff18582918
Rasmus <rasmus.grey@hotmail.se>
2023-12-14 22:31:00 +0100

    AdventOfCode 2023 day1 & day2

---
+++ code-01/src/main.rs
@@ -0,0 +1,88 @@
+fn main() {
+    let input = include_str!("../input").lines().collect::<Vec<_>>();
+    main::advent_of_code_day_1(input);
+}
+
+pub mod main {
+
+    pub fn advent_of_code_day_1(input: Vec<&str>) -> i32 {
+        let array_strings = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+
+        let mut sum = 0;
+
+        for line in input {
+            let mut num = String::new();
+            let mut first = (0, 0);
+            let mut last = (0, 0);
+            let mutable_line = line.to_string();
+
+            let mut first_word = (0, 0);
+            let mut last_word = (0, 0);
+
+            for (index, word) in array_strings.iter().enumerate() {
+                if mutable_line.contains(word) {
+                    let word_index = mutable_line.find(word).unwrap();
+                    if word_index < first_word.1 || first_word.0 == 0 {
+                        first_word = (index + 1, word_index as usize);
+                    }
+                    let word_index = mutable_line.rfind(word).unwrap();
+                    if word_index > last_word.1 || last_word.0 == 0 {
+                        last_word = (index + 1, word_index as usize);
+                    }
+                }
+            }
+
+            for (index, character) in mutable_line.chars().enumerate() {
+                if character.is_numeric() {
+                    if first.0 == 0 {
+                        first = (character.to_digit(10).unwrap(), index);
+                    } else {
+                        last = (character.to_digit(10).unwrap(), index);
+                    }
+                }
+            }
+
+            if last.0 == 0 {
+                last = first;
+            }
+            if (first.1 > first_word.1 && first_word.0 != 0) || first.0 == 0 {
+                first = (first_word.0 as u32, first_word.1);
+            }
+            if last.1 < last_word.1 || (last.0 == 0 && first.0 != 0) {
+                last = (last_word.0 as u32, last_word.1);
+            }
+            num.push(first.0.to_string().chars().next().unwrap());
+            num.push(last.0.to_string().chars().next().unwrap());
+
+            println!("{}", line);
+            println!("answer: {}", num);
+
+            sum += num.parse::<i32>().unwrap();
+        }
+
+        println!("Sum of numbers in the vector is {}", sum);
+
+        return sum;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    fn test_advent_of_code_day_1(#[case] line: &str, #[case] expected: i32) {
+        assert_eq!(main::advent_of_code_day_1(vec![line]), expected);
+    }
+}
---
+++ code-02/src/main.rs
@@ -0,0 +1,214 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Config {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+fn day2_part1(input: Vec<&str>, config: Config) -> i32 {
+    // split input line on
+    let mut sum = 0;
+    for game in input {
+        let game_name = game.split(":").next().unwrap();
+        let game_id = game_name
+            .split(" ")
+            .skip(1)
+            .next()
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+
+        let mut possible = true;
+
+        let split = game.split(":").skip(1).next().unwrap().trim();
+        for set in split.split("; ") {
+            for roll in set.split(", ").into_iter() {
+                let split_roll = roll.split(" ").collect::<Vec<&str>>();
+                let num = split_roll[0].parse::<i32>().unwrap();
+                let color = split_roll[1];
+
+                possible = match color {
+                    "red" => config.red >= num,
+                    "green" => config.green >= num,
+                    "blue" => config.blue >= num,
+                    _ => false,
+                };
+                if !possible {
+                    break;
+                }
+            }
+            if !possible {
+                break;
+            }
+        }
+        if possible {
+            println!("{}: possible", game_id);
+            println!("{}", game);
+            sum += game_id;
+            println!("sum: {}", sum);
+        } else {
+            println!("{}: impossible", game_id);
+            println!("{}", game);
+            println!("sum: {}", sum);
+        }
+    }
+    sum
+}
+
+fn day2_part2(input: Vec<&str>) -> i32 {
+    let mut total_power = 0;
+    for game in input {
+        let game_name = game.split(":").next().unwrap();
+        let game_id = game_name
+            .split(" ")
+            .skip(1)
+            .next()
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+
+        let mut lowest_possible = Config {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+
+        let split = game.split(":").skip(1).next().unwrap().trim();
+        for set in split.split("; ") {
+            for roll in set.split(", ").into_iter() {
+                let split_roll = roll.split(" ").collect::<Vec<&str>>();
+                let num = split_roll[0].parse::<i32>().unwrap();
+                let color = split_roll[1];
+                match color {
+                    "red" => {
+                        if lowest_possible.red < num {
+                            lowest_possible.red = num;
+                        }
+                    }
+                    "green" => {
+                        if lowest_possible.green < num {
+                            lowest_possible.green = num;
+                        }
+                    }
+                    "blue" => {
+                        if lowest_possible.blue < num {
+                            lowest_possible.blue = num;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        total_power += lowest_possible.red * lowest_possible.green * lowest_possible.blue;
+    }
+    total_power
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1)]
+    #[case("game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2)]
+    #[case(
+        "game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        0
+    )]
+    #[case(
+        "game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        0
+    )]
+    #[case("game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5)]
+    fn test_lines(#[case] line: &str, #[case] possible: i32) {
+        let config = Config {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        assert_eq!(day2_part1(vec![line], config), possible);
+    }
+
+    #[test]
+    fn sum() {
+        let config = Config {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        let lines = vec![
+            "game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ];
+
+        assert_eq!(day2_part1(lines, config), 8);
+    }
+
+    #[test]
+    fn real_input_sum() {
+        let config = Config {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        let lines = include_str!("../input").lines().collect::<Vec<_>>();
+
+        println!(
+            "Sum of numbers in the vector is {}",
+            day2_part1(lines, config)
+        );
+    }
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    #[rstest]
+    #[case("game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48)]
+    #[case("game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12)]
+    #[case(
+        "game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        1560
+    )]
+    #[case(
+        "game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        630
+    )]
+    #[case("game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)]
+    fn part_2_test_lines(#[case] line: &str, #[case] expected: i32) {
+        let tot = day2_part2(vec![line]);
+        println!("{}: {}", line, tot);
+        assert_eq!(tot, expected);
+    }
+
+    #[test]
+    fn part_2_test_input() {
+        let lines = vec![
+            "game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ];
+
+        assert_eq!(day2_part2(lines), 2286);
+    }
+
+    #[test]
+    fn part_2_real_input() {
+        let lines = include_str!("../input").lines().collect::<Vec<_>>();
+
+        println!("Total power: {}", day2_part2(lines));
+    }
+}
